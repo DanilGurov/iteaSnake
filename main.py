f2e0b3c60ab1c96feed2aab1891afac2246fc231
MisterYuriy <veremeevu@gmail.com>
2017-12-30 11:47:03 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ def forward():
     move.forward(10)
     turtle.ontimer(forward, 200)
 
+def a():
+    return random.randint(-300, 300)
+
+# Create apple
+x, y = t.position()
+t.up()
+t.setpos(a(), a())
+t.dot(10, 'green')
+t.goto(x, y)
 
 move.up()
 turtle.ontimer(forward, 200)
@@ -20,3 +29,5 @@ turtle.onkey(left, 'a')
 turtle.onkey(right, 'd')
 turtle.listen()
 turtle.mainloop()
+
+
